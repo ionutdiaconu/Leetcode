1c1d5bf7c6c508d9a0a9f5fada21358a96c590d3
ionut diaconu <ionut.diaconu@gmail.com>
2023-02-24 23:44:14 +0200

    Two sumsAdd 2 numbers using Linked List

---
+++ AddTwoNumbersLinkedList/Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+using System.Numerics;
+
+namespace Test
+{
+
+    class Solution
+    {
+        static void Main()
+        {
+            Solution s = new Solution();
+
+            ListNode l1 = s.IntToNode(9);
+            ListNode l2 = s.IntToNode(1999999999);
+
+            ListNode result = s.AddTwoNumbers(l1, l2);
+
+            Console.WriteLine("Result: {0}", s.NodeToNumber(result));
+        }
+
+        private ListNode GetNode(char[] valArray)
+        {
+
+            if (valArray.Length == 0)
+            {
+                return null;
+            }
+
+            char[] newValArray = valArray[1..];
+
+            return new()
+            {
+                val = int.Parse("" + valArray[0]),
+                next = GetNode(newValArray),
+            };
+
+        }
+
+        private ListNode IntToNode(BigInteger val)
+        {
+            char[] valArray = val.ToString().ToCharArray();
+            Array.Reverse(valArray);
+
+            return GetNode(valArray);
+        }
+
+
+        private BigInteger NodeToNumber(ListNode node)
+        {
+            string val = "";
+            ListNode current = node;
+
+            while (current != null)
+            {
+                val += "" + current.val;
+                current = current.next;
+            }
+
+            char[] valArray = val.ToCharArray();
+            Array.Reverse(valArray);
+
+            string nodeValueStrig = new(valArray);
+            Console.WriteLine("nodeValueStrig:" + nodeValueStrig);
+
+            return BigInteger.Parse(nodeValueStrig);
+
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            BigInteger sum = NodeToNumber(l1) + NodeToNumber(l2);
+            return IntToNode(sum);
+        }
+    }
+}
---
+++ AddTwoNumbersLinkedList2/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+using System.Numerics;
+
+namespace Test
+{
+
+    class Solution
+    {
+        static void Main()
+        {
+            Solution s = new Solution();
+
+            //ListNode l1 = new ListNode(9);
+            //ListNode l2 = new ListNode(1, new ListNode(9));
+
+            ListNode l1 = new ListNode(5);
+            ListNode l2 = new ListNode(7, new ListNode(4));
+
+            ListNode result = s.AddTwoNumbers(l1, l2);
+
+            Console.WriteLine("Result: {0}", result);
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode ();
+            int carry = 0;
+            ListNode cur = result;
+
+            while (l1 != null || l2 != null || carry > 0)
+            {
+
+                int val1 = l1 != null ? l1.val : 0;
+                int val2 = l2 != null ? l2.val : 0;
+
+                int thisVal = val1 + val2 + carry;
+
+                carry = thisVal / 10;
+                thisVal %= 10;
+
+
+                cur.next = new ListNode (thisVal);
+                cur = cur.next;
+                
+
+
+                l1 = l1 != null ? l1.next : null;
+                l2 = l2 != null ? l2.next : null;
+
+            }
+            return result.next;
+
+        }
+    }
+}
